4692b64aee41143f7e82eded031d8cf281bb9985
Garvit Jain <93989695+nellgar1111@users.noreply.github.com>
2023-09-07 11:04:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,14 @@ import streamlit as st
 import pandas as pd
 import xgboost as xgb
 
+import matplotlib.pyplot as plt
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+
+from sklearn.decomposition import PCA
+from sklearn.svm import SVC
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
 #import pickle
 #Loading up the Regression model we created
 model = xgb.XGBClassifier()
